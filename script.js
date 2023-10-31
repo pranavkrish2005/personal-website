b63530e5b8213a8d02d421af720409af3cf650cd
Pranav Krishnan <pranavkrish2005.gmail.com>
2023-10-30 21:17:01 -0400

    added javasacript

--- script.js
+++ script.js
@@ -1,16 +1,26 @@
-function returnHi() {
-    return "Hi!";
-}
+// function returnHi() {
+//     return "Hi!";
+// }
 
-let titleElement = documentl.getElementById("title");
+// let titleElement = documentl.getElementById("title");
 
-function changeTitle() {
-titleElement.innerHTML = returnHi();
-}
+// function changeTitle() {
+// titleElement.innerHTML = returnHi();
+// }
 
-setTimeout(changeTitle, 2000);
+// setTimeout(changeTitle, 2000);
 
-document.getElementById("title").addEventListener("click", () => {
-    alert("You clicked the title!");
-    document.getElementById("title").style.color = "red";
+// document.getElementById("title").addEventListener("click", () => {
+//     alert("You clicked the title!");
+//     document.getElementById("title").style.color = "red";
+// });
+
+document.getElementById("about").addEventListener("click", () => {
+    document.getElementById("about").style.color = "blue";
+});
+document.getElementById("projects").addEventListener("click", () => {
+    document.getElementById("projects").style.color = "blue";
+});
+document.getElementById("contact").addEventListener("click", () => {
+    document.getElementById("contact").style.color = "blue";
 });
